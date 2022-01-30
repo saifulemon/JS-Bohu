9bdcbfa61ec66ea173726a44366534b724b4c41e
saifulemon <463saiful0@gmail.com>
2022-01-30 20:57:51 +0600

    Error Handling Process added

---
+++ practiceJS/Error Handling/error.js
@@ -0,0 +1,33 @@
+// Error Handling
+/*
+console.log('Before Error!');
+
+try {
+    test();
+    undefined.test();
+}
+
+catch(err) {
+    console.log(err);
+    console.log(err.message);
+    console.log(err.name);
+}
+
+finally {
+    console.log('I am inside finally!');
+}
+
+console.log('After Error!');
+*/
+
+
+let a = 16;
+
+try {
+    if (a > 15) throw 'Too Big';
+    else if (a < 24) throw 'Too small';
+}
+
+catch (err) {
+    console.log(err);
+}
